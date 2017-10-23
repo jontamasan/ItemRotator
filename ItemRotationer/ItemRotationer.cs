e3b00a27a862ad8ccad8d6c49446a50114f38cab
jontamasan <levelsync@gmail.com>
2017-10-23 21:23:58 +0900

    プロジェクト ファイルを追加します。

---
+++ ItemRotationer/ItemRotationer.cs
@@ -0,0 +1,29 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace ItemRotationer
+{
+    public class ItemRotationer : Mod
+    {
+        public override string ID => "ItemRotationer";
+        public override string Name => "ItemRotationer";
+        public override string Author => "Your Username";
+        public override string Version => "1.0";
+
+        //Set this to true if you will be load custom assets from Assets folder.
+        //This will create subfolder in Assets folder for your mod.
+        public override bool UseAssetsFolder => false;
+
+        //Called when mod is loading
+        public override void OnLoad()
+        {
+
+        }
+
+        // Update is called once per frame
+        public override void Update()
+        {
+
+        }
+    }
+}
