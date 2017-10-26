173f15de0ebb51b09ac995081271d341d33cf933
jontamasan <levelsync@gmail.com>
2017-10-27 00:40:32 +0900

    change to Assenbly information

--- ItemRotationer/Properties/AssemblyInfo.cs
+++ ItemRotationer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ItemRotator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MSC items rotation on all axes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ItemRotator")]
